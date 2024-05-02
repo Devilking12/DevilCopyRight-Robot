8349712b2cf0af4cb77ce4c5cf7221d404360383
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-05-02 21:58:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 from os import getenv
 
-API_ID = int(os.environ.get("API_ID"))
+API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 BOT_USERNAME = os.environ.get("BOT_USERNAME")
